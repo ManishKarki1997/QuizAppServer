0102791da9f3e4dafaee57f805e650ca3a96d90c
ManishKarki1997 <manishkarki247@gmail.com>
2020-06-04 23:35:20 +0545

    game room setup

--- socket/userSocket.js
+++ socket/userSocket.js
@@ -1,73 +1,22 @@
 const { QuestionModel } = require('../models');
+const { v4: uuidv4 } = require('uuid');
 
 let users = []; // holds all online users in a class
 let allActiveUsers = {}; // holds all the online users
-const gameCountdown = 5
-
-const questions = {
-    topic: 'Science',
-    data: [
-        {
-            question: {
-                question: 'Which is the closest star to the Earth?',
-                options: ['Sun', 'Proxima Centauri', 'Betelgeuse', 'North Star'],
-                answer: 'Sun'
-            }
-        },
-        {
-            question: {
-                question: 'Which of the following is used in pencils?',
-                options: ['Graphite', 'Silicon', 'Charcoal', 'Phosphorous'],
-                answer: 'Graphite'
-            }
-        },
-        {
-            question: {
-                question: 'The gas usually filled in the electric bulb is ',
-                options: ['Hydrogen', 'Carbon Dioxide', 'Oxygen', 'Nitrogen'],
-                answer: 'Nitrogen'
-            }
-        },
-        {
-            question: {
-                question: 'Washing soda is the common name for',
-                options: [
-                    'Calcium BiCarbonate',
-                    'Sodium Carbonate',
-                    'Calcium Carbonate',
-                    'Sodium BiCarbonate'
-                ],
-                answer: 'Sodium BiCarbonate'
-            }
-        },
-        {
-            question: {
-                question: 'Which of the gas is not a Green House gas?',
-                options: ['Methane', 'Nitrous Oxide', 'Carbon Dioxide', 'Hydrogen'],
-                answer: 'Hydrogen'
-            }
-        },
-        {
-            question: {
-                question:
-                    'The inert gas which is substituted for nitrogen in the air used by deep sea divers for breathing is',
-                options: ['Argon', 'Xenon', 'Helium', 'Krypton'],
-                answer: 'Helium'
-            }
-        },
-        {
-            question: {
-                question: 'The average salinity of sea water is',
-                options: ['3%', '3.5%', '2.5%', '2%'],
-                answer: '3.5%'
-            }
-        }
-    ]
-}
+const gameCountdown = 1
+const answerCountdown = 15;
+let gameRooms = {}
 
 const userSockets = (io) => {
 
     io.on('connection', (socket) => {
+
+        // io.clients((error, clients) => {
+        //     if (error) throw error;
+        //     clients.forEach(client => {
+        //         console.log(client + ' is connedted? ' + io.sockets.connected[client].id);
+        //     })
+        // });
 
         // helper function to emit all online users
         const emitAllUsers = () => {
@@ -86,35 +35,119 @@ const userSockets = (io) => {
         socket.on("SUBMIT_USER_DETAILS", (data) => {
             allActiveUsers[socket.id] = {
                 ...data,
-                socketId: socket.id
+                socketId: socket.id,
+                // socketFunction: socket.join
             }
+            // socket.to(socket.id).emit('YOUR_DETAILS', allActiveUsers[socket.id]);
             emitAllUsers();
         })
 
         socket.on("START_GAME", (data) => {
-            const { roomName, challenger, opponent } = data;
+            const roomName = uuidv4();
+            const { challenger, opponent } = data;
 
             // join both user sockets to the room
-            io.sockets.connected[challenger.socketId].join(roomName);
-            io.sockets.connected[opponent.socketId].join(roomName);
+            if (io.sockets.connected[challenger.socketId])
+                io.sockets.connected[challenger.socketId].join(roomName);
 
-            // send the two users details to each other
+            if (io.sockets.connected[opponent.socketId])
+                io.sockets.connected[opponent.socketId].join(roomName);
+
+
+
+            // send the two users' details to each other
             // on the client side, they'll figure out who their opponent is
+            // socket.emit("OPPONENT_DETAILS", [
+            //     challenger,
+            //     opponent,
+            //     roomName
+            // ])
+
+
             io.to(roomName).emit("OPPONENT_DETAILS", [
                 challenger,
-                opponent
+                opponent,
+                roomName
             ]);
 
             // emit the game starting countdown
-            socket.emit('GAME_IN_SECONDS', gameCountdown)
+            // socket.emit('GAME_IN_SECONDS', gameCountdown)
+            io.to(roomName).emit('GAME_IN_SECONDS', gameCountdown)
 
             // after the countdown, emit the game questions
             setTimeout(() => {
                 QuestionModel.findRandom({ categoryId: '5ed7ddf90161e65078a89f08' }, {}, { limit: 10, populate: 'categoryId' }, function (err, results) {
+
+
                     io.to(roomName).emit("GAME_QUESTIONS", results);
+
+                    // setup a room for the players. one room has two players only
+                    gameRooms[roomName] = {
+                        challenger: {
+                            points: 0,
+                            ...challenger,
+                            answerPattern: [],
+                            lastAnswerCorrect: false,
+                            answerCountdown
+                        },
+                        opponent: {
+                            points: 0,
+                            ...opponent,
+                            answerPattern: [],
+                            lastAnswerCorrect: false,
+                            answerCountdown
+                        },
+                        questionIndex: 0,
+                        gameQuestions: results,
+                        miscDetails: {
+                            gameDraw: false,
+                            gameWonBy: '',
+                            questionIndex: 0,
+                            gameOver: false,
+                        }
+                    }
+
                 });
 
             }, gameCountdown * 1000);
+
+        })
+
+        // Game Manager
+        socket.on("GAME_MANAGER", (data) => {
+            const { answerer, questionIndex, roomName, answer } = data;
+
+
+            if (gameRooms[roomName].gameQuestions[questionIndex].answer === answer) {
+                if (gameRooms[roomName].challenger.socketId === answerer.socketId) {
+                    gameRooms[roomName].challenger.points++;
+                    gameRooms[roomName].challenger.lastAnswerCorrect = true;
+                    gameRooms[roomName].challenger.answerPattern.push("W");
+
+                }
+                else {
+                    gameRooms[roomName].opponent.points++;
+                    gameRooms[roomName].opponent.lastAnswerCorrect = true;
+                    gameRooms[roomName].opponent.answerPattern.push("W");
+                }
+                // gameRooms[roomName].
+            } else {
+                if (gameRooms[roomName].challenger.socketId === answerer.socketId) {
+                    gameRooms[roomName].challenger.lastAnswerCorrect = false;
+                    gameRooms[roomName].challenger.answerPattern.push("L");
+                } else {
+                    gameRooms[roomName].opponent.lastAnswerCorrect = false;
+                    gameRooms[roomName].opponent.answerPattern.push("L");
+                }
+            }
+
+            // send the gameroom status to the players 
+            // console.log(io.sockets.adapter.rooms[roomName])
+            // socket.emit("ANSWER_RESULT", gameRooms[roomName]);
+            io.to(roomName).emit("ANSWER_RESULT", gameRooms[roomName]);
+
+            // increment the gameroom question to next question
+            gameRooms[roomName].questionIndex++;
 
         })
 
